e786006e2d57e17e81ae60e9a611f02a7b3e6798
ELBORIS28Ipro <borisfotabong@gmail.com>
2023-06-23 19:27:03 -0700

    this is for my ecommerce tutorial

--- my_project/ecommerce/static/js/cart.js
+++ my_project/ecommerce/static/js/cart.js
@@ -8,15 +8,43 @@ for (i = 0; i < updateBtns.length; i++){
 
      console.log('USER:', user)
      if (user == 'AnonymousUser'){
-     console.log('your not logged in')
+         addCookieItem(productId,action)
      }else{
           updateUserOrder(productId,action)
      }
      })
  }
+
+ function addCookieItem(productId,action){
+       console.log('user is not authenticated')
+
+       if(action == 'add'){
+          if(cart[productId] == undefined){
+          cart[productId] = {'quantity':1}
+
+          }else{
+              cart[productId]['quantity'] += 1
+          }
+       }
+       if(action == 'remove'){
+           cart[productId]['quantity'] -= 1
+
+           if(cart[productId]['quantity'] <= 0){
+              console.log('removed item')
+              delete cart[productId]
+           }
+       }
+       console.log('Cart:', cart)
+       document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+       location.reload()
+
+
+ }
+
+
  function updateUserOrder(productId,action){
        console.log('User is logged in, sending data....')
-       var url = 'update_item/'
+       var url = '/update_item/'
 
        fetch(url, {
        method:'POST',
@@ -24,12 +52,13 @@ for (i = 0; i < updateBtns.length; i++){
             'Content-Type':'application/json',
             'X-CSRFToken':csrftoken,
              },
-             body:JSON.stringify({'productId': productId,'action:' :action})
+             body:JSON.stringify({'productId': productId,'action' :action})
          })
          .then((response) =>{
              return response.json()
            })
          .then((data) =>{
              console.log('data:', data)
-           })
+             location.reload()
+           });
   }
--- my_project/ecommerce/store/views.py
+++ my_project/ecommerce/store/views.py
@@ -1,37 +1,89 @@
 from django.shortcuts import render
 from.models import *
 from django.http import JsonResponse
+import json
+import datetime
+from.utilis import cookieCart, cartData, guestOrder
 
-# Create your views here.
 def store(request):
+    #we should have used a rest API to add values numbers of product to cart,but this is manually
+    data = cartData(request)
+    cartItems = data['cartItems']
+
+     #the manual method ends here and 'cartItem' variable will be added to the other functions
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store.html', context)
 
 def cart(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
-    context = {'items': items, 'order': order}
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'cart.html', context)
 
 def checkout(request):
+     data = cartData(request)
+     cartItems = data['cartItems']
+     order = data['order']
+     items = data['items']
+
+     context = {'items': items, 'order': order, 'cartItems': cartItems}
+     return render(request, 'checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('action:', action)
+    print('productId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('item was added', safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
+
     else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        customer,order = guestOrder(request, data)
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
 
-    context = {'items': items, 'order': order}
-    return render(request, 'checkout.html', context)
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
 
-def updateItem(request):
-    return JsonResponse('item was added', safe=False)
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+
+        )
+    return JsonResponse('payment complete!', safe=False)
 
